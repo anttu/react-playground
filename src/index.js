fc5c7c408468f311d9bc91a3ca07d29086f277cf
Antero Fagerstedt <antero.fagerstedt@iki.fi>
2016-09-26 19:21:11 +0300

    applied the css template to react code

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import Template from './Template';
 import './index.css';
 
 ReactDOM.render(
-    <App />,
+    <Template />,
     document.getElementById('root')
 );
